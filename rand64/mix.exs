8403fb1d01708748b240b34ed4466aa5e50e29cb
Y. Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2021-09-09 20:39:16 +0300

    Publish to hex.pm

--- rand64/mix.exs
+++ rand64/mix.exs
@@ -6,6 +6,14 @@ defmodule Rand64.MixProject do
       app: :rand64,
       version: "0.1.0",
       elixir: "~> 1.12",
+      description: "Simple and stupid Elixir library for generating random strings.",
+      source_url: "https://github.com/CadmiumC4/rand64.git",
+      package: [
+      	name: "randomie64",
+	licenses: ["GPL-3.0-only"],
+	links: []
+	],
+      
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,6 +31,7 @@ defmodule Rand64.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
